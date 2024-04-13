20fd0bffe3e4f613cf8a67a0a2dd8a63cc9b6afd
vikas-kumar-09 <vikashgupta1234x@gmail.com>
2024-04-13 19:58:17 +0530

    first commit

---
+++ Visualizer/Main.java
@@ -0,0 +1,9 @@
+
+ package Visualizer;
+
+public class Main {
+     public static void main(String[] args) {
+        SortingVisualizer.frame = new VisualizerFrame();
+        SortingVisualizer.frame.setLocationRelativeTo(null);
+     }    
+}
